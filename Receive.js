f2a2e1b86594b0f522109a234864d0f6a596e5e0
seigo2016 <2003seigo@gmail.com>
2019-05-31 20:51:28 +0900

    Update Receive.js

--- Receive.js
+++ Receive.js
@@ -1,9 +1,9 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "02") {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 1)
         pins.digitalWritePin(DigitalPin.P8, 1)
         pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P16, 1)
         basic.showLeds(`
             . # # # #
             # . # . #
@@ -13,7 +13,7 @@ radio.onReceivedString(function (receivedString) {
             `)
     }
     if (receivedString == "01") {
-        pins.servoWritePin(AnalogPin.P16, 0)
+        pins.servoWritePin(AnalogPin.P1, 0)
         basic.showLeds(`
             . # . # .
             # . # . #
@@ -23,7 +23,7 @@ radio.onReceivedString(function (receivedString) {
             `)
     }
     if (receivedString == "00") {
-        pins.servoWritePin(AnalogPin.P16, 55)
+        pins.servoWritePin(AnalogPin.P1, 55)
         basic.showLeds(`
             . # . . #
             # . # # #
@@ -33,10 +33,10 @@ radio.onReceivedString(function (receivedString) {
             `)
     }
     if (receivedString == "1") {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
         pins.digitalWritePin(DigitalPin.P8, 1)
         pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
         basic.showLeds(`
             . . # . .
             . # # . .
@@ -46,10 +46,10 @@ radio.onReceivedString(function (receivedString) {
             `)
     }
     if (receivedString == "2") {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 1)
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
         basic.showLeds(`
             . # # # .
             . . . # .
@@ -59,10 +59,10 @@ radio.onReceivedString(function (receivedString) {
             `)
     }
     if (receivedString == "3") {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P8, 1)
         pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
         basic.showLeds(`
             . # # # .
             . . . # .
@@ -72,37 +72,23 @@ radio.onReceivedString(function (receivedString) {
             `)
     }
     if (receivedString == "4") {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
         basic.showLeds(`
             . # . # .
             . # . # .
             . # # # .
             . . . # .
             . . . # .
-            `)
-    }
-    if (receivedString == "5") {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . . . # .
-            . # # # .
             `)
     }
 })
 radio.setGroup(1)
-pins.servoSetPulse(AnalogPin.P16, 1500)
-pins.servoWritePin(AnalogPin.P16, 0)
-pins.analogWritePin(AnalogPin.P0, 1023)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P2, 0)
+pins.servoSetPulse(AnalogPin.P1, 1500)
+pins.servoWritePin(AnalogPin.P1, 0)
 pins.digitalWritePin(DigitalPin.P8, 0)
 pins.digitalWritePin(DigitalPin.P12, 0)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P16, 0)
